c9aa0186958d7772592de56c4520a4208e757fd5
copiousfreetime <jeremy@copiousfreetime.org>
2018-03-14 12:32:03 -0600

    add byebug history manifest ignore

--- tasks/this.rb
+++ tasks/this.rb
@@ -28,7 +28,7 @@ class ThisProject
     @exclude_from_manifest = Regexp.union(/\.(git|DS_Store)/,
                                           /^(doc|coverage|pkg|tmp|Gemfile(\.lock)?)/,
                                           /^[^\/]+\.gemspec/,
-                                          /\.(swp|jar|bundle|so|rvmrc|travis.yml)$/,
+                                          /\.(swp|jar|bundle|so|rvmrc|travis.yml|byebug_history)$/,
                                           /~$/)
     @gemspecs              = Hash.new
     yield self if block_given?
