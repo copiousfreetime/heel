7c18ab56fb13919f4ca1f3a7c3a2f43db879b8cd
copiousfreetime <jeremy@copiousfreetime.org>
2023-04-10 18:06:25 -0600

    mime types got updated

--- spec/directory_indexer_spec.rb
+++ spec/directory_indexer_spec.rb
@@ -31,15 +31,18 @@ describe Heel::DirectoryIndexer do
     _(@indexer.mime_map).must_be_instance_of(Heel::MimeMap)
   end
 
-  [ { :ext => 'svg', :type => 'image/svg+xml' },
+  [
+    { :ext => 'svg', :type => 'image/svg+xml' },
     { :ext => 'flv', :type => 'video/x-flv' },
-    { :ext => 'swf', :type => 'application/x-shockwave-flash' } ].each do |m|
+    { :ext => 'rb', :type => 'application/x-ruby' },
+    { :ext => 'rhtml', :type => 'application/x-html+ruby' }
+  ].each do |m|
       it "finds #{m[:ext]} extension in the map as #{m[:type]}" do
         _(@indexer.mime_map.mime_type_of( "test.#{m[:ext]}" )).must_equal m[:type]
       end
     end
 
-  %w[ md markdown rdoc rb rhtml ].each do |ext|
+  %w[ md markdown rdoc ].each do |ext|
     it "finds #{ext} in the map as text/plain" do
       _(@indexer.mime_map.mime_type_of( "test.#{ext}" )).must_equal "text/plain"
     end
