2a982719519f00672941f210422e3859f8fc4d09
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-05-04 17:13:16 -0600

    add documentation

--- lib/heel/directory_entry.rb
+++ lib/heel/directory_entry.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Heel
+  # Internal: Strure for holding display information on a directory entry
+  #
   DirectoryEntry = Struct.new(:name, :link, :last_modified, :content_type,
                               :display_size, :icon_url, :mime_type,
                               keyword_init: true)
