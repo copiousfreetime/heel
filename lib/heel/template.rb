c3a5a0bddb8f8e92f79cfc5f1d9c8bb93f48d512
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-05-05 00:29:14 -0600

    add template base class

---
+++ lib/heel/template.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "erb"
+
+module Heel
+  # Internal: A wrapper for an erb template
+  #
+  class Template
+    attr_reader :path, :template, :loaded_at
+
+    def initialize(path)
+      @path = path
+      @loaded_at = nil
+      @template = load_template
+    end
+
+    def should_reload?
+      loaded_at < last_modified
+    end
+
+    def load_template
+      @loaded_at = Time.now
+     ::ERB.new(File.read(path))
+    end
+
+    def last_modified
+      File.stat(path).mtime
+    end
+
+    def render(binding)
+      reload_template if should_reload?
+      template.result(binding)
+    end
+  end
+end
+
