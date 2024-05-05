bae6f956cdd5a772c16c95b5f06e1f3013cd9b34
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-05-04 20:12:26 -0600

    fixup for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require "simplecov"
-SimpleCov.start if ENV["COVERAGE"]
-
-gem "minitest"
 require "heel"
 require "minitest/autorun"
+require "minitest/focus"
 require "minitest/pride"
