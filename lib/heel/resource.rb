5e5efc973b999489afd82ab658ce32ef10792e49
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-05-05 00:29:22 -0600

    add a resource and response base class

---
+++ lib/heel/resource.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+#--
+# Copyright (c) 2007 - 2013 Jeremy Hinegardner
+# All rights reserved. Licensed under the BSD license. See LICENSE for details
+#++
+
+require "marcel"
+
+module Heel
+  # Internal: Resource representing a single file or directory that is being served
+  #
+  class Resource
+    class << self
+      def icons_by_content_type
+        @icons_by_mime_type ||= Hash.new("file.svg").tap do |hash|
+          hash.update({
+            "image" => "image.svg",
+            "pdf" => "pdf.svg",
+            "x-zip-compressed" => "zip.svg",
+            "x-gtar" => "zip.svg",
+            "x-gzip" => "zip.svg",
+            "application/x-word" => "word.svg",
+            "application/powerpoint" => "presentation.svg",
+            "text/html" => "html5.svg",
+            :directory => "folder-alt.svg",
+          })
+        end
+      end
+    end
+
+    attr_reader :path, :magic, :stat
+
+    def initialize(path:)
+      @path = path
+      @stat = File.stat(@path)
+      @magic = calculate_magic(@path) unless directory?
+    end
+
+    def last_modified
+      stat.mtime.strftime("%Y-%m-%d %H:%M:%S")
+    end
+
+    def size
+      stat.size
+    end
+
+    def directory?
+      stat.directory?
+    end
+
+    def icon_slug
+      key = directory? ? :directory : content_type
+      self.class.icons_by_mime_type[key]
+    end
+
+    def content_type
+      directory? ? 'Directory' : (magic&.type&.downcase || 'application/octet-stream')
+    end
+
+    def calculate_magic(path)
+      magic = nil
+      with_io(path) do |io|
+        magic = Marcel::Magic.by_magic(io)
+      end
+      magic ||= Marcel::Magic.by_path(path)
+    end
+
+    def with_io(path = @path)
+      File.open(path, "rt:bom|utf-8") do |io|
+        yield io
+      end
+    end
+  end
+end
---
+++ lib/heel/response.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module Heel
+  # Internal: A wrapper for a rack response
+  #
+  class Response
+    attr_reader :request, :options, :response
+
+    def self.homepage
+      @homepage ||= Heel::Configuration::HOMEPAGE
+    end
+
+    # Initialize the request with the environment and the root directory of the
+    # request
+    def initialize(request:, options: {}, status: 200, headers: { "Content-Type" => "text/html" })
+      @request = request
+      @options = options
+      @response = Rack::Response.new("", status, headers)
+    end
+
+    def should_ignore?(fname)
+      options[:ignore_globs].each do |glob|
+        return true if ::File.fnmatch(glob, fname)
+      end
+      false
+    end
+
+    def base_uri
+      request.base_uri
+    end
+
+    def request_path
+      request.request_path
+    end
+
+    def highlighting?
+      options.fetch(:highlighting, false)
+    end
+
+    def using_icons?
+      options.fetch(:using_icons, false)
+    end
+
+    def icon_base_url
+      options.fetch(:icon_url, "/heel_icons")
+    end
+
+    def homepage
+      self.class.homepage
+    end
+  end
+end
